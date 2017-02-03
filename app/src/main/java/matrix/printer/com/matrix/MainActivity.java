d861faf35c56a6fc4bbd7ab636b502bf6a964a92
simon <simon@cs4africa.com>
2017-02-03 11:54:20 +0300

    App push

---
+++ app/src/main/java/matrix/printer/com/matrix/MainActivity.java
@@ -0,0 +1,266 @@
+package matrix.printer.com.matrix;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends Activity {
+
+    // will show the statuses
+    TextView myLabel;
+
+    // will enable user to enter any text to be printed
+    EditText myTextbox;
+
+    // android built in classes for bluetooth operations
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            // we are goin to have three buttons for specific functions
+            Button openButton = (Button) findViewById(R.id.open);
+            Button sendButton = (Button) findViewById(R.id.send);
+            Button closeButton = (Button) findViewById(R.id.close);
+
+            myLabel = (TextView) findViewById(R.id.label);
+            myTextbox = (EditText) findViewById(R.id.entry);
+
+            // open bluetooth connection
+            openButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    try {
+                        findBT();
+                        openBT();
+                    } catch (IOException ex) {
+                    }
+                }
+            });
+
+            // send data typed by the user to be printed
+            sendButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    try {
+                        sendData();
+                    } catch (IOException ex) {
+                    }
+                }
+            });
+
+            // close bluetooth connection
+            closeButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    try {
+                        closeBT();
+                    } catch (IOException ex) {
+                    }
+                }
+            });
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // This will find a bluetooth printer device
+    void findBT() {
+
+        try {
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            if (mBluetoothAdapter == null) {
+                myLabel.setText("No bluetooth adapter available");
+            }
+
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBluetooth = new Intent(
+                        BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBluetooth, 0);
+            }
+
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
+                    .getBondedDevices();
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+
+                    // MP300 is the name of the bluetooth printer device
+                    if (device.getName().equals("Printer")) {
+                        mmDevice = device;
+                        break;
+                    }
+                }
+            }
+            myLabel.setText("One Bluetooth Device Found. Opened");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Tries to open a connection to the bluetooth printer device
+    void openBT() throws IOException {
+        try {
+            // Standard SerialPortService ID
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            mmSocket.connect();
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+
+            beginListenForData();
+
+            //myLabel.setText("Bluetooth Opened");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // After opening a connection to bluetooth printer device,
+    // we have to listen and check if a data were sent to be printed.
+    void beginListenForData() {
+        try {
+            final Handler handler = new Handler();
+
+            // This is the ASCII code for a newline character
+            final byte delimiter = 10;
+
+            stopWorker = false;
+            readBufferPosition = 0;
+            readBuffer = new byte[1024];
+
+            workerThread = new Thread(new Runnable() {
+                public void run() {
+                    while (!Thread.currentThread().isInterrupted()
+                            && !stopWorker) {
+
+                        try {
+
+                            int bytesAvailable = mmInputStream.available();
+                            if (bytesAvailable > 0) {
+                                byte[] packetBytes = new byte[bytesAvailable];
+                                mmInputStream.read(packetBytes);
+                                for (int i = 0; i < bytesAvailable; i++) {
+                                    byte b = packetBytes[i];
+                                    if (b == delimiter) {
+                                        byte[] encodedBytes = new byte[readBufferPosition];
+                                        System.arraycopy(readBuffer, 0,
+                                                encodedBytes, 0,
+                                                encodedBytes.length);
+                                        final String data = new String(
+                                                encodedBytes, "US-ASCII");
+                                        readBufferPosition = 0;
+
+                                        handler.post(new Runnable() {
+                                            public void run() {
+                                                myLabel.setText(data);
+                                            }
+                                        });
+                                    } else {
+                                        readBuffer[readBufferPosition++] = b;
+                                    }
+                                }
+                            }
+
+                        } catch (IOException ex) {
+                            stopWorker = true;
+                        }
+
+                    }
+                }
+            });
+
+            workerThread.start();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * This will send data to be printed by the bluetooth printer
+     */
+    void sendData() throws IOException {
+        try {
+            String msg = myTextbox.getText().toString();
+            final String msg2 = "TEXT";
+            byte k = 9;
+            byte HORIZONTALTAB = 9;
+            byte LINEFEED = 10;
+            byte[] VERTICALTAB = {11};
+            byte[] SELECT_SIX_LINES_PER_INCH = {27,50};
+            byte[] SELECT_EIGHT_LINES_PER_INCH = {27,48};
+            byte[] SELECT_VERTICAL_TAB_CHANNEL = {27,47,0x63};
+            String newLineA = " ";
+            String newLineB = " ";
+            String newLineC = " ";
+            String newLineD = " ";
+            String newLineE = msg;
+            byte buf= 0x0A;
+            for (int i = 0; i<16; i++){
+                mmOutputStream.write(LINEFEED);
+            }
+            for(int j = 0; j <4; j++){
+                mmOutputStream.write(HORIZONTALTAB);
+            }
+            mmOutputStream.write(newLineE.getBytes());
+            mmOutputStream.write(LINEFEED);
+
+
+            myLabel.setText("Data Sent");
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Close the connection to bluetooth printer.
+    void closeBT() throws IOException {
+        try {
+            stopWorker = true;
+            mmOutputStream.close();
+            mmInputStream.close();
+            mmSocket.close();
+            myLabel.setText("Bluetooth Closed");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
